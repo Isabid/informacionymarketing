13e989b842dd2efcd6a0e99e92d0f8ed1c43333e
Isabid Gutierrez P <isabid@gmail.com>
2024-01-13 14:33:32 -0500

    ajuste a movil safari

--- iym.js
+++ iym.js
@@ -1,7 +1,6 @@
-
 // JavaScript para desplazarse al inicio de la página cuando se hace clic en el enlace
 document.getElementById('regresarAlInicio').addEventListener('click', function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
 // Obtén todas las imágenes ampliables
@@ -9,12 +8,12 @@ const imagenesAmpliables = document.querySelectorAll('.ampliable');
 
 // Agrega un evento de clic a cada imagen ampliable
 imagenesAmpliables.forEach((imagen) => {
-let escalado = 1; // Factor de escala inicial
-const factorDeIncremento = 0.2; // Cuánto incrementar el tamaño con cada clic
+    let escalado = 1; // Factor de escala inicial
+    const factorDeIncremento = 0.2; // Cuánto incrementar el tamaño con cada clic
 
-imagen.addEventListener('click', (event) => {
-            // Verifica si fue un clic normal o un doble clic
-            if (event.detail === 1) {
+    imagen.addEventListener('click', (event) => {
+        // Verifica si fue un clic normal o un doble clic
+        if (event.detail === 1) {
             // Incrementa el factor de escala
             escalado += factorDeIncremento;
             // Limita el tamaño máximo al 200% del tamaño original (ajusta según tus necesidades)
@@ -23,29 +22,29 @@ imagen.addEventListener('click', (event) => {
             // Aplica el nuevo tamaño con una transición suave
             imagen.style.transition = 'transform 0.3s ease';
             imagen.style.transform = `scale(${escalado})`;
-            } else {
+        } else {
             // Doble clic, restablece el tamaño original
             escalado = 1;
             imagen.style.transition = 'transform 0.3s ease';
             imagen.style.transform = `scale(${escalado})`;
-            }
+        }
+    });
 });
-});
- 
-// dar una funcioalidad especial para safari
+
+// Dar una funcionalidad especial para safari
 let clickedOnce = false;
 
 document.getElementById('tuElemento').addEventListener('click', function () {
-        if (clickedOnce) {
-            // Manejar doble clic
-            // Restaura al tamaño original
-            this.style.width = '300px'; // Ajusta según tu tamaño original
-        } else {
-            clickedOnce = true;
-            setTimeout(function () {
-                clickedOnce = false;
-            }, 300); // Ajusta el tiempo para detectar doble clic
-        }
+    if (clickedOnce) {
+        // Manejar doble clic
+        // Restaura al tamaño original
+        this.style.width = '300px'; // Ajusta según tu tamaño original
+    } else {
+        clickedOnce = true;
+        setTimeout(function () {
+            clickedOnce = false;
+        }, 300); // Ajusta el tiempo para detectar doble clic
+    }
 });
 
 document.querySelectorAll('nav a').forEach(function(enlace) {
